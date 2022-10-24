ceb97aaaed5be4b7bdab111830d05131c5a1b6ec
ginnanikasuos <e.onzuka@gmail.com>
2022-10-24 20:59:15 +0900

    Initial Commit

---
+++ LiveTextChecker/LiveTextCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTextCheckerApp.swift
+//  LiveTextChecker
+//
+//  Created by 隠塚永治 on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveTextCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
